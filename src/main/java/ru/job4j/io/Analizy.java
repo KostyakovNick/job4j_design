9d5ae8e695acc356b5d0fd7e6b85bc08cc07a5af
Николай <kostnn@list.ru>
2022-09-17 16:47:33 +0700

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,36 +7,31 @@ import java.util.stream.Collectors;
 public class Analizy {
 
     public void unavailable(String source, String target) {
-        List<String> list = read(source);
-        List<String> rsl = new ArrayList<>();
-        String status = "";
-        String time = "";
-        for (String s : list) {
-            s = s.trim();
-            String[] strings = s.split(" ");
-            if (("200".equals(status) || "300".equals(status))
-                    && ("400".equals(strings[0]) || "500".equals(strings[0]))) {
-                time = time.concat(strings[1] + ";");
-            }
-            if (("400".equals(status) || "500".equals(status))
-                    && ("200".equals(strings[0]) || "300".equals(strings[0]))) {
-                time = time.concat(strings[1] + ";");
-                rsl.add(time);
-                time = "";
-            }
-            status = strings[0];
-        }
-        save(rsl, target);
-    }
-
-    public List<String> read(String source) {
-        List<String> log = new ArrayList<>();
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            log = read.lines().filter(line -> !line.startsWith("#") && !line.isEmpty()).collect(Collectors.toList());
+            List<String> rsl = new ArrayList<>();
+            String status = "";
+            String time = "";
+            for (String line = read.readLine(); line != null; line = read.readLine()) {
+                if (!line.startsWith("#") && !line.isEmpty()) {
+                    line = line.trim();
+                    String[] strings = line.split(" ");
+                    if (("200".equals(status) || "300".equals(status))
+                            && ("400".equals(strings[0]) || "500".equals(strings[0]))) {
+                        time = time.concat(strings[1] + ";");
+                    }
+                    if (("400".equals(status) || "500".equals(status))
+                            && ("200".equals(strings[0]) || "300".equals(strings[0]))) {
+                        time = time.concat(strings[1] + ";");
+                        rsl.add(time);
+                        time = "";
+                    }
+                    status = strings[0];
+                }
+            }
+            save(rsl, target);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return log;
     }
 
     public static void save(List<String> log, String file) {
@@ -53,7 +48,7 @@ public class Analizy {
     }
 
     public static void main(String[] args) {
-        String source = "./data/server.log";
+        String source = "./data/server1.log";
         String target = "./data/unavailable.csv";
         Analizy analizy = new Analizy();
         analizy.unavailable(source, target);
