e4048f45becf10ebffe8a6d0deb4e293a24b922a
Николай <kostnn@list.ru>
2022-09-18 18:19:42 +0700

    4.2. Поиск дубликатов [#315066 #344650]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,14 +5,11 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private final Map<FileProperty, List<Path>> rsl = new HashMap<>();
+    private static Map<FileProperty, List<Path>> rsl = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
@@ -23,13 +20,22 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
             List<Path> tmpPaths = rsl.get(fileProperty);
             tmpPaths.add(file.toAbsolutePath());
             rsl.put(fileProperty, tmpPaths);
-            System.out.printf("%s - %.2fMb\n", fileProperty.getName(), (double) fileProperty.getSize() / 1024);
-            for (Path p : tmpPaths) {
-                System.out.println(p);
-            }
         } else {
             rsl.put(fileProperty, paths);
         }
         return super.visitFile(file, attrs);
+    }
+
+    public static void print() {
+        Iterator<Map.Entry<FileProperty, List<Path>>> itr = rsl.entrySet().iterator();
+        while (itr.hasNext()) {
+            Map.Entry<FileProperty, List<Path>> entry = itr.next();
+            if (entry.getValue().size() > 1) {
+                System.out.printf("%s - %.2fMb\n", entry.getKey().getName(), (double) entry.getKey().getSize() / 1024);
+                for (Path p : entry.getValue()) {
+                    System.out.println(p);
+                }
+            }
+        }
     }
 }
