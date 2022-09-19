c439dbaffd1203ec11ebb6dec6c492cd44f26302
Николай <kostnn@list.ru>
2022-09-19 16:31:52 +0700

    5. Валидация параметров запуска. [#246865 #344665]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,14 +10,20 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("No arguments!");
+        }
         check(args);
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static void check(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("No arguments!");
+        if (!new File(args[0]).isDirectory()) {
+            throw new IllegalArgumentException("No directory!");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Does not contain the sing \".\"");
         }
     }
 
